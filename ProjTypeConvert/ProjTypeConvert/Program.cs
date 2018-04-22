cad3c7727428e1b9453b3f4c5327a64c240dbce5
Oleksiy Nekrasov <n.507592979@gmail.com>
2018-04-22 12:23:36 +0200

    d

---
+++ ProjTypeConvert/ProjTypeConvert/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjTypeConvert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+
+            if(input.All(char.IsNumber))
+            {
+                int number = Convert.ToInt32(input);
+                number = number + 1000;
+                Console.WriteLine(number);
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
